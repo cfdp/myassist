e5847d5fcb62913957543e2a6922c029828b17a7
Anita Jensen <ahj@novicell.dk>
2020-07-06 12:28:55 +0200

    adding deployer

---
+++ deploy.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Deployer;
+
+$config_file = 'deployer/config.yml';
+if (!is_file($config_file) || !is_readable($config_file)) {
+  echo 'Could not find configuration file';
+  exit;
+}
+
+// Include recipes
+require 'deployer/recipe/base.php';
+
+// Load config file
+inventory($config_file);
+
